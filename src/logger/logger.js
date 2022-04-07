393bf5fb92439925419cfe4509f7b4fbb8197c57
abhishekchaurasiya <abhichek2580@gmail.com>
2022-04-07 19:23:46 +0530

    All Change

--- src/logger/logger.js
+++
@@ -1,7 +0,0 @@
-let welcome = function () {
-    let print = "Welcome to my application. I am < Abhishek Chaurasiya > and a part of FunctionUp Thorium cohort."
-    console.log(print);
-}
-
-module.exports = { welcome }
-// module.exports.print = print
