af648b1a7cec8f27438e2514f8f957b1ec8ff7c6
abhishekchaurasiya <abhichek2580@gmail.com>
2022-04-12 02:47:13 +0530

    create new simple book library project

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 const express = require('express');
-var bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
+
 
 const route = require('./routes/route.js');
+const mongoose = require('mongoose');
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+
+mongoose.connect("mongodb+srv://mongoabhishek:JGETcKMFq8k1RFrV@cluster0.nn6fz.mongodb.net/myBookName?retryWrites=true&w=majority", {useNewUrlParser : true}).then(() => console.log('mongo is connected')).catch(error => console.log(error))
+
+
 app.use('/', route);
 
-app.listen(process.env.PORT || 4000, function() {
+app.listen(process.env.PORT || 4000, function () {
     console.log('Express app running on port ' + (process.env.PORT || 4000))
 });
--- src/routes/route.js
+++ src/routes/route.js
@@ -2,49 +2,10 @@ const express = require('express');
 
 const router = express.Router();
 
-let players = [
-    {
-        "name": "gopal",
-        "dob": "1/09/1995",
-        "gender": "male",
-        "city": "delhi",
-        "sports": [
-            "soccer"
-        ],
-    },
-
-    {
-        "name": "lokesh",
-        "dob": "1/1/1990",
-        "gender": "male",
-        "city": "mumbai",
-        "sports": [
-            "soccer"
-        ],
-    },
-]
 
 router.post("/players", function (req, res) {
 
-    let allData = req.body
-    let returnData = 0;
-    players.filter((x) => {
-        if (x.name === allData.name) {
-            returnData = 1;
-        }
-    })
-    if (returnData === 1) {
-        return res.send({ message: "This name is already exists" });
-    } else {
-        players.push(allData)
-        console.log(players);
-
-    }
-    // res.send(players
-
-    res.send({ data: players, status: true })
-
-    // res.send('hello')
+    res.send('hello')
 })
 
 
